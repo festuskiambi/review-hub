1d8d3d54277411909911618227cb95c6cbd47c06
festus <kiamfestoh@gmail.com>
2019-04-09 09:36:47 +0300

    add validations to review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_061139) do
+ActiveRecord::Schema.define(version: 2019_04_09_063504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_04_09_061139) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["customer_id"], name: "index_reviews_on_customer_id"
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { should belong_to(:product) }
   it { should belong_to(:customer) }
+  it { should validate_length_of(:comment).is_at_least(20) }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 end
