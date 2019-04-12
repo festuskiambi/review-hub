a3b500595365ceddfc8bda9177bf4a5e355c5b61
festus <kiamfestoh@gmail.com>
2019-04-12 12:40:17 +0300

    create seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :customers, controllers: { registrations: 'registrations' }
+  resources :products do
+    resources :issues
+    resources :reviews
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #
 #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #  Character.create(name: 'Luke', movie: movies.first)
+Customer.destroy_all
+Product.destroy_all
+customer = Customer.create!(first_name: 'firstname', user_name: 'username',
+                            email: 'example@email.com', password: '123456',
+                            password_confirmation: '123456')
+product = Product.create!(name: 'product1', code: 'QWER', category: 'Apparel')
+review = Review.create!(rating: 3, comment: 'not good nood  f  dd  d d s  s  ',
+                        customer: customer, product: product)
+issue = Issue.create!(comment: 'not responding to this event now!',
+                      severity: 'MINOR', product: product, customer: customer)
